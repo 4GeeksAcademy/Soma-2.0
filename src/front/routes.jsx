746801b18122afa41903e619faf9a78590cff5d0
jhunadev <jhunalbisdeyanquintero@gmail.com>
2026-08-18 02:50:56 +0000

    feat: implementar UI de lista de pacientes

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -1,9 +1,4 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Navigate,
-  Route
-} from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Navigate, Route } from "react-router-dom";
 
 import { Layout } from "./pages/Layout";
 import { Landing } from "./pages/Landing";
@@ -16,49 +11,30 @@ import { Espacios } from "./pages/Espacios";
 import { Agenda } from "./pages/Agenda";
 import { NuevoServicio } from "./pages/NuevoServicio";
 import { ProtectedRoute } from "./components/ProtectedRoute";
+import { ListaPacientes } from "./pages/ListaPacientes";
 
 export const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route
-        path="/"
-        element={<Landing />}
-        errorElement={<h1>Not found!</h1>}
-      />
+	createRoutesFromElements(
+		<>
+			<Route path="/" element={<Landing />} errorElement={<h1>Not found!</h1>} />
 
-      <Route
-        path="/login"
-        element={<Login />}
-        errorElement={<h1>Not found!</h1>}
-      />
+			<Route path="/login" element={<Login />} errorElement={<h1>Not found!</h1>} />
 
-      <Route
-        path="/olvide-password"
-        element={<OlvidePassword />}
-        errorElement={<h1>Not found!</h1>}
-      />
+			<Route path="/olvide-password" element={<OlvidePassword />} errorElement={<h1>Not found!</h1>} />
 
-      <Route
-        path="/restablecer-password"
-        element={<RestablecerPassword />}
-        errorElement={<h1>Not found!</h1>}
-      />
+			<Route path="/restablecer-password" element={<RestablecerPassword />} errorElement={<h1>Not found!</h1>} />
 
-      <Route
-        path="/app"
-        element={<ProtectedRoute />}
-        errorElement={<h1>Not found!</h1>}
-      >
-        <Route element={<Layout />}>
-          <Route index element={<Navigate to="/app/agenda" replace />} />
-          <Route path="single/:theId" element={<Single />} />
-          <Route path="demo" element={<Demo />} />
-          <Route path="espacios" element={<Espacios />} />
-          <Route path="agenda" element={<Agenda />} />
-          <Route path="nuevo-servicio" element={<NuevoServicio />} />
-        </Route>
-      </Route>
-    </>
-  )
+			<Route path="/app" element={<ProtectedRoute />} errorElement={<h1>Not found!</h1>}>
+				<Route element={<Layout />}>
+					<Route index element={<Navigate to="/app/agenda" replace />} />
+					<Route path="single/:theId" element={<Single />} />
+					<Route path="demo" element={<Demo />} />
+					<Route path="espacios" element={<Espacios />} />
+					<Route path="agenda" element={<Agenda />} />
+					<Route path="pacientes" element={<ListaPacientes />} />
+					<Route path="nuevo-servicio" element={<NuevoServicio />} />
+				</Route>
+			</Route>
+		</>
+	)
 );
-
