a6833f4a8e0e8a9454a7fb03ca44bf42fe474650
Frankk <frankgrc96@gmail.com>
2026-08-19 15:10:58 +0000

    Merge branch 'origin/main' into feature/dashboard-ingresos

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter, createRoutesFromElements, Navigate, Route } from "react-router-dom";
 
-import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import { Layout } from "./pages/Layout";
 import { Landing } from "./pages/Landing";
 import { Login } from "./pages/Login";
@@ -13,9 +12,9 @@ import { Agenda } from "./pages/Agenda";
 import { DashboardAdmin } from "./pages/DashboardAdmin";
 import { AppIndexRedirect } from "./components/AppIndexRedirect";
 import { NuevoServicio } from "./pages/NuevoServicio";
-import { ListaPacientes } from "./pages/ListaPacientes";
+import { ListaPacientes } from "./pages/ ListaPacientes";
 import { ProtectedRoute } from "./components/ProtectedRoute";
-import { ListaPacientes } from "./pages/ListaPacientes";
+
 
 export const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -43,30 +42,4 @@ export const router = createBrowserRouter(
 	)
 );
 
-    createRoutesFromElements(
-        <>
-            {/* Landing pública */}
-            <Route path="/" element={<Landing />} errorElement={<h1>Not found!</h1>} />
-
-            {/* Auth */}
-            <Route path="/login" element={<Login />} errorElement={<h1>Not found!</h1>} />
-            <Route path="/olvide-password" element={<OlvidePassword />} errorElement={<h1>Not found!</h1>} />
-            <Route path="/restablecer-password" element={<RestablecerPassword />} errorElement={<h1>Not found!</h1>}/>
-
-            {/* App Autenticada */}
-                <Route path="/app" element={<ProtectedRoute />} errorElement={<h1>Not found!</h1>}>
-                    <Route element={<Layout />}>
-                        {/* Redirección inteligente por rol: Admin -> Dashboard | Asistente/Especialista -> Agenda */}
-                        <Route index element={<AppIndexRedirect />} />
-                        <Route path="dashboard" element={<DashboardAdmin />} />
-                        <Route path="agenda" element={<Agenda />} />
-                        <Route path="pacientes" element={<ListaPacientes />} />
-                        <Route path="nuevo-servicio" element={<NuevoServicio />} />
-                        <Route path="espacios" element={<Espacios />} />
-                        <Route path="single/:theId" element={<Single />} />
-                        <Route path="demo" element={<Demo />} />
-                    </Route>
-                </Route>
-            </>
-        )
-    );
+    
