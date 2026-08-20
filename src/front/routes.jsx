039b4bca104958ff0895ebb3f260ccabb36eec39
Frankk <frankgrc96@gmail.com>
2026-08-20 01:59:37 +0000

    fix: se arreglo el error de importacion de AppIndexRedirect y DashboardAdmin

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createRoutesFromElements, Navigate, Route } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 
 import { Layout } from "./pages/Layout";
 import { Landing } from "./pages/Landing";
@@ -12,7 +12,7 @@ import { Agenda } from "./pages/Agenda";
 import { DashboardAdmin } from "./pages/DashboardAdmin";
 import { AppIndexRedirect } from "./components/AppIndexRedirect";
 import { NuevoServicio } from "./pages/NuevoServicio";
-import { ListaPacientes } from "./pages/ ListaPacientes";
+import { ListaPacientes } from "./pages/ListaPacientes";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 
 
@@ -29,13 +29,18 @@ export const router = createBrowserRouter(
 
 			<Route path="/app" element={<ProtectedRoute />} errorElement={<h1>Not found!</h1>}>
 				<Route element={<Layout />}>
-					<Route index element={<Navigate to="/app/agenda" replace />} />
+					<Route index element={<AppIndexRedirect />} />
+					<Route path="dashboard" element={<DashboardAdmin />} />
 					<Route path="single/:theId" element={<Single />} />
 					<Route path="demo" element={<Demo />} />
 					<Route path="espacios" element={<Espacios />} />
 					<Route path="agenda" element={<Agenda />} />
 					<Route path="pacientes" element={<ListaPacientes />} />
 					<Route path="nuevo-servicio" element={<NuevoServicio />} />
+
+					
+
+
 				</Route>
 			</Route>
 		</>
